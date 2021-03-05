b85f27cb1e4738da1c08e9537d267003074bdaef
Axel Caille <axel@quasyl.com>
2021-03-05 17:27:25 +0100

    Adds connection to MongDB and tasks.html template.

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
+
+# Database configuration/Get database name
+app.config["MONGODB_NAME"] = os.environ.get("MONGO_DBNAME")
+# Database configuration/Get database string
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# Flask configuration/Get secret key
+app.secret_key = os.environ.get("SECRET_KEY")
+# Database configuration/Pymongo instance
+mongo = PyMongo(app)
 
 
 @app.route("/")
-def hello_world():
-    return "Hello World!"
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.pdca_tasks.find()
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
