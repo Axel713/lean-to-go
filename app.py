660e3e09f12c4d0791874bf35e224fe3d5adb84f
Axel Caille <axel@quasyl.com>
2021-03-07 17:58:28 +0100

    Adds Login functionality

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def get_tasks():
 
 
 @app.route("/register", methods=["GET", "POST"])
+# register new user
 def register():
     if request.method == "POST":
         # check username exists in db
@@ -51,6 +52,32 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration complete!")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # verify hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # if provided password does not match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # if provided username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
