a76932ffd7ab6df3eaf9ff5998864b3f98a45ce2
Axel Caille <axel@quasyl.com>
2021-03-08 13:39:54 +0100

    Adds input fields to add task form

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: 'right'});
-    $(".dropdown-trigger").dropdown();
+    $('.dropdown-trigger').dropdown();
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 2,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
